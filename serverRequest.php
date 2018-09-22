57cb663d7caffca661b8c96f03a6a64e484d95dd
Jason Cheng <jasoncheng224@gmail.com>
2018-09-22 16:41:28 -0700

    Removed extra files again

--- serverRequest.php
+++
@@ -1,6 +0,0 @@
-<?php
-  // The global $_POST variable allows you to access the data sent with the POST method by name
-  // To access the data sent with the GET method, you can use $_GET
-  $actor = htmlspecialchars($_POST["actor"]);
-  echo  $actor;
-?>
